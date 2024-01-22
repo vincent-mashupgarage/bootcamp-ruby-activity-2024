b9d63da64a4e91d23bf6daaa1f9d37e4a70aed10
vincent-mashupgarage <155512730+vincent-mashupgarage@users.noreply.github.com>
2024-01-22 14:55:46 +0800

    ruby activity
    ruby activity

--- arena.rb
+++ arena.rb
@@ -1,40 +1,72 @@
 class Arena
   # 3. add a reader for both player and enemy
-  # enter code here
+ # enter code here
+ attr_reader :player, :enemy
 
-  def initialize(player:, enemy:)
+ def initialize(player:, enemy:)
     # 2. Create an instance variable for player and enemy
-    # enter code here
-  end
+   # enter code here
+   @player = player
+   @enemy = enemy
+ end
 
-  def start
-    puts "Welcome to the Arena!"
-    puts "You are fighting #{enemy.name}!"
-    while player.hp > 0 && enemy.hp > 0
+ def start
+   puts "Welcome to the Arena!"
+   puts "You are fighting #{enemy.name}!"
+   while player.hp > 0 && enemy.hp > 0
       # 4. Implement actions where Player and Enemey will both fight. Player and Enemy will do a random action.
-      # ACTIONS are: attack and use_potion
-      # enter code here
+     # ACTIONS are: attack and use_potion
+     # enter code here
+     player_action
+     enemy_action
 
-      puts "Your health: #{player.hp}"
-      puts "#{enemy.name}'s health: #{enemy.hp}"
-    end
+     puts "Your health: #{player.hp}"
+     puts "#{enemy.name}'s health: #{enemy.hp}"
+   end
 
-    if player.hp > 0
-      puts "You win!"
-    else
-      puts "You lose!"
-    end
-  end
+   if player.hp > 0
+     puts "You win!"
+   else
+     puts "You lose!"
+   end
+ end
 
-  private
+ private
 
-    def attack(attacker, target)
-      damage = attacker.atk - target.def
-      target.hp -= damage
+ def attack(attacker, target)
+   damage = attacker.atk - target.def.to_i
+   target.hp -= damage
 
-      puts "#{attacker.name} attacks #{target.name} for #{damage} damage!"
-    end
+   puts "#{attacker.name} attacks #{target.name} for #{damage} damage!"
+ end
 
-    # 5.Challenge: Create a method where player can defend
-    # Specs: Defending can make the defender's defense x1.5 in 1 turn
+ def player_action
+   puts "Choose action (1: Attack, 2: Use Potion):"
+   action = gets.chomp.to_i
+
+   case action
+   when 1
+     attack(player, enemy)
+   when 2
+     player.use_potion
+   else
+     puts "Invalid action. Try again."
+     player_action
+   end
+ end
+
+ def enemy_action
+   actions = [:attack, :use_potion]
+   random_action = actions.sample
+
+   case random_action
+   when :attack
+     attack(enemy, player)
+   when :use_potion
+     enemy.use_potion
+   end
+ end
+
+ # 5.Challenge: Create a method where player can defend
+   # Specs: Defending can make the defender's defense x1.5 in 1 turn
 end
--- player.rb
+++ player.rb
@@ -1,5 +1,6 @@
 class Player
   MAX_POTIONS_QTY = 5.freeze
+  POTION_HEAL_AMOUNT = 15
 
   attr_accessor :name, :potions, :hp
   attr_reader :atk, :def
@@ -8,16 +9,37 @@ class Player
     @name = name
     @hp = 100
     @atk = 20
-    @def = 5
     @potions = MAX_POTIONS_QTY
+    @def_bonus = 1.0
   end
 
-  # 1. Modiify this method where a player can heal using potions and reduce the player's potion amount
-  # Specs: Using potion can heal 15 hp
+  # 1. Create a method where the player can heal using potions and reduce the player's potion amount
+  # Specs: Using a potion can heal 15 hp
   def use_potion
-    # Prevent using of potions when no more potions left. Hint: use guard clause here
-    # enter code here
+    # Prevent using potions when no more potions left. Use guard clause here.
+    return puts "#{name} has no left potion" if potions <= 0
 
-    puts "#{name} used potions."
+    @hp += POTION_HEAL_AMOUNT
+    @hp = [@hp, 100].min
+
+    @potions -= 1
+
+    puts "#{name} used a potion."
+    puts "#{POTION_HEAL_AMOUNT} HP healed."
+    puts "Remaining potions: #{potions}. Current HP: #{hp}."
   end
 end
+
+# player = Player.new("Victor")
+
+# puts "#{player.name} stat"
+# puts "HP: #{player.hp}"
+# puts "Potions: #{player.potions}"
+
+# player.use_potion
+
+# puts "\n#{player.name} stat after using the potion:"
+# puts "HP: #{player.hp}"
+# puts "Potions: #{player.potions}"
+
+
